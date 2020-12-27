92c3403ad02fb0856fbed334d461effd0d2b5494
viralbanda <viralbandachannel@gmail.com>
2020-12-27 15:50:08 +0000

    RMX1851: Switch to AOSP BT implementation
    Reference: https://github.com/ArrowOS-Devices/android_device_asus_X00T/commit/e55eccbc92be5e0086ded66fe5dd3a39b8866d82
    Signed-off-by: viralbanda <viralbandachannel@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -9,12 +9,16 @@ endif
 
 # Audio
 PRODUCT_PRODUCT_PROPERTIES += \
-   ro.config.vc_call_vol_steps=7 \
-   ro.config.media_vol_steps=25
+   ro.bluetooth.a2dp_offload.supported=false \
+   persist.bluetooth.a2dp_offload.disabled=true \
+   persist.bluetooth.bluetooth_audio_hal.disabled=true \
+   vendor.audio.feature.a2dp_offload.enable=false \
+   persist.vendor.qcom.bluetooth.enable.splita2dp=false \
+   persist.vendor.bt.a2dp.aac_whitelist=false
 
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
-   vendor.bluetooth.soc=cherokee
+   ro.bluetooth.library_name=libbluetooth.so
 
 # Blurs
 PRODUCT_PRODUCT_PROPERTIES += \
